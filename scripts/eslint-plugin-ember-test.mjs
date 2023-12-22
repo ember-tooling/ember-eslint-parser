65fca1c00e88d458269f7bee7b5bc4463475da91
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-22 17:29:47 -0500

    execa works better than zx, apparently

--- scripts/eslint-plugin-ember-test.mjs
+++ scripts/eslint-plugin-ember-test.mjs
@@ -1,23 +1,18 @@
-/* globals $, within */
-import 'zx/globals';
+import { execaCommand } from 'execa';
+import fse from 'fs-extra';
 
+const REPO = `https://github.com/ember-cli/eslint-plugin-ember.git`;
 const FOLDERS = {
+  here: process.cwd(),
   testRoot: '/tmp/eslint-plugin-ember-test/',
+  repo: '/tmp/eslint-plugin-ember-test/eslint-plugin-ember',
 };
 
-await $`yarn link`;
+await fse.remove(FOLDERS.testRoot);
+await fse.ensureDir(FOLDERS.testRoot);
 
-within(async () => {
-  await $`mkdir -p ${FOLDERS.testRoot}`;
-  await $`cd ${FOLDERS.testRoot}`;
-
-  await $`git clone https://github.com/ember-cli/eslint-plugin-ember.git`;
-  await $`cd eslint-plugin-ember`;
-  await $`yarn link ember-eslint-parser`;
-
-  // debug
-  await $`ls -la node_modules | grep ember-eslint-parser`;
-
-  // run tests
-  await $`yarn test`;
-});
+await execaCommand(`git clone ${REPO}`, { cwd: FOLDERS.testRoot, stdio: 'inherit' });
+await execaCommand(`yarn install`, { cwd: FOLDERS.repo, stdio: 'inherit' });
+await execaCommand(`yarn link`, { cwd: FOLDERS.here, stdio: 'inherit' });
+await execaCommand(`yarn link ember-eslint-parser`, { cwd: FOLDERS.repo, stdio: 'inherit' });
+await execaCommand(`yarn run test`, { cwd: FOLDERS.repo, stdio: 'inherit' });
