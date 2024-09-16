2aa80d5469cbbe318f5f8dad173a280eece39e98
Brad Overton <bradley.overton@3plearning.com>
2024-09-16 12:48:24 +1000

    chore: see if script/test succeeds on specific commit of eslint-plugin-ember

--- scripts/eslint-plugin-ember-test.mjs
+++ scripts/eslint-plugin-ember-test.mjs
@@ -13,6 +13,10 @@ await fse.ensureDir(FOLDERS.testRoot);
 
 // Using pnpm instead of yarn, because pnpm is way faster
 await execaCommand(`git clone ${REPO}`, { cwd: FOLDERS.testRoot, stdio: 'inherit' });
+await execaCommand(`git checkout 8f485b2198ce143ccbf88768fa36316d82cc36ae`, {
+  cwd: FOLDERS.repo,
+  stdio: 'inherit',
+});
 await execaCommand(`pnpm install`, { cwd: FOLDERS.repo, stdio: 'inherit' });
 await execaCommand(`pnpm add ${FOLDERS.here}`, { cwd: FOLDERS.repo, stdio: 'inherit' });
 await execaCommand(`pnpm run test`, { cwd: FOLDERS.repo, stdio: 'inherit' });
