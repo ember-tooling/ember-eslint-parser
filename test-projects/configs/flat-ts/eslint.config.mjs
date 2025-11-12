497b4cbd3bdcdb82118c3be95a37058919c8066a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-12 01:52:03 -0500

    ope

--- test-projects/configs/flat-ts/eslint.config.mjs
+++ test-projects/configs/flat-ts/eslint.config.mjs
@@ -25,7 +25,7 @@ import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 const manifestPath = require.resolve('@typescript-eslint/parser/package.json');
 const manifest = require(manifestPath);
-const isV8 = manifest.version[0] > 8;
+const isV8 = parseInt(manifest.version[0]) >= 8;
 
 const parserOptions = {
   esm: {
