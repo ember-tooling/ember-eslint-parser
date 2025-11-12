7f9354186307f9e4eecd9ccc19a215d6b91e3ed3
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-12 08:11:26 -0500

    ope

--- test-projects/gjs-types/.eslintrc.cjs
+++ test-projects/gjs-types/.eslintrc.cjs
@@ -3,12 +3,12 @@
 const path = require('path');
 
 const parserOptions = {};
-if (process.env.PROJECT_SERVICE) {
+// if (process.env.PROJECT_SERVICE) {
   parserOptions.projectService = {
-    defaultProject: process.env.DEFAULT_PROJECT || 'tsconfig.json',
+    defaultProject: process.env.DEFAULT_PROJECT || process.env.PROJECT || 'tsconfig.json',
     allowDefaultProject: process.env.ALLOW_DEFAULT_PROJECT === 'true' ? ['**/*.js'] : []
   };
-}
+// }
 
 parserOptions.tsconfigRootDir = process.env.TSCONFIG_ROOT_DIR || __dirname;
 parserOptions.project = process.env.PROJECT === undefined ? true : process.env.PROJECT;
