832a4a2ac5a38b4430ce1ddf16edb51a743e329c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-12 08:31:41 -0500

    ope

--- test-projects/gts/.eslintrc.cjs
+++ test-projects/gts/.eslintrc.cjs
@@ -1,9 +1,5 @@
 'use strict';
 
-
-parserOptions.tsconfigRootDir = process.env.TSCONFIG_ROOT_DIR || __dirname;
-parserOptions.project = process.env.PROJECT === undefined ? true : process.env.PROJECT;
-
 const manifestPath = require.resolve('@typescript-eslint/parser/package.json');
 const manifest = require(manifestPath);
 const isV8 = parseInt(manifest.version[0]) >= 8;
