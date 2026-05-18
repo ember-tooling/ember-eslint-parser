a1956c9c9cc7fe1d8071b5c4cca43cbe3bc2697d
David Taylor <david@taylorhq.com>
2026-05-18 09:29:32 +0000

    feat: support @babel/eslint-parser v8
    `@babel/eslint-parser` v8 promoted the 'experimental worker' as the default behavior, so we need to update the import.
    
    Also widens the peer-dep range and adds a `gjs-babel-v8` test project mirroring `gjs-experimental-worker` so CI exercises both paths.

---
+++ test-projects/gjs-babel-v8/babel.config.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  plugins: [
+    [
+      'babel-plugin-ember-template-compilation',
+      {
+        compilerPath: 'ember-source/dist/ember-template-compiler.js',
+        enableLegacyModules: [
+          'ember-cli-htmlbars',
+          'ember-cli-htmlbars-inline-precompile',
+          'htmlbars-inline-precompile',
+        ],
+        transforms: [],
+      },
+    ],
+    [
+      'module:decorator-transforms',
+      {
+        runtime: {
+          import: require.resolve('decorator-transforms/runtime-esm'),
+        },
+      },
+    ],
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        absoluteRuntime: __dirname,
+        useESModules: true,
+        regenerator: false,
+      },
+    ],
+  ],
+
+  generatorOpts: {
+    compact: false,
+  },
+};
+
